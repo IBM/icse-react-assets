0b88ba33b8c6f9c8c3cecb96afaf37236f04fdc5
jvallexm <jvallexm@gmail.com>
2023-02-06 15:55:20 -0500

    feat: object storage key form

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,25 +1,9 @@
 import "./App.css";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import { ToggleFormExample } from "./examples/ToggleForm";
->>>>>>> f680a35 (almost done)
-=======
-import { SshKeyFormExample } from "./examples/SshKeyForm";
->>>>>>> 09dbacf (SshKeyForm, example, and docs)
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-<<<<<<< HEAD
       {/* test resources here */}
-=======
-      <ToggleFormExample />
->>>>>>> f680a35 (almost done)
-=======
-      <SshKeyFormExample />
->>>>>>> 09dbacf (SshKeyForm, example, and docs)
     </div>
   );
 }
