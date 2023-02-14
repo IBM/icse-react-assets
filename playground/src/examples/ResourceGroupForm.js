f1c055e63bcbb04bdea2430aa4d58968f90babdf
Daniel Oestreicher <daniel.oestreicher@ibm.com>
2023-02-14 11:48:19 -0600

    add ResourceGroupForm example

---
+++ playground/src/examples/ResourceGroupForm.js
@@ -0,0 +1,12 @@
+import { ResourceGroupForm } from "icse-react-assets";
+
+export const ResourceGroupFormExample = () => {
+  return (
+    <ResourceGroupForm
+      data={{
+        name: "example-form",
+      }}
+      isModal={true}
+    />
+  );
+};
