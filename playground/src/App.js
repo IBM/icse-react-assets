af85370e747287cb6a31082b512f084315e454c9
jvallexm <jvallexm@gmail.com>
2023-04-04 12:36:11 -0400

    feat: form

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,76 +1,67 @@
 import React from "react";
-import { VsiForm } from "icse-react-assets";
+import {
+  VsiLoadBalancerForm
+} from "icse-react-assets";
 import "./App.css";
 
+function validName(str) {
+  const regex = /^[A-z]([a-z0-9-]*[a-z0-9])?$/i;
+  if (str) return str.match(regex) !== null;
+  else return false;
+}
+
+function invalidCallback(stateData, componentProps) {
+  return !validName(stateData.name);
+}
+
+function invalidTextCallback(stateData, componentProps) {
+  return !validName(stateData.name)
+    ? `Name ${stateData.name} is invalid.`
+    : `Invalid Name. Must match the regular expression: /^[A-z]([a-z0-9-]*[a-z0-9])?$/i`;
+}
+
 function App() {
-  function validName(str) {
-    const regex = /^[A-z]([a-z0-9-]*[a-z0-9])?$/i;
-    if (str) return str.match(regex) !== null;
-    else return false;
-  }
-
-  function invalidCallback(stateData, componentProps) {
-    return !validName(stateData.name);
-  }
-
-  function invalidTextCallback(stateData, componentProps) {
-    return !validName(stateData.name)
-      ? `Name ${stateData.name} is invalid.`
-      : `Invalid Name. Must match the regular expression: /^[A-z]([a-z0-9-]*[a-z0-9])?$/i`;
-  }
-
   return (
-    <VsiForm
-      resourceGroups={["test", "foo", "bar"]}
-      subnetList={[
-        {
-          vpc: "management",
-          zone: 1,
-          cidr: "10.10.10.0/24",
-          name: "vsi-zone-1",
-          network_acl: "management",
-          resource_group: "management-rg",
-          public_gateway: false,
-          has_prefix: true,
-        },
-        {
-          vpc: "management",
-          zone: 1,
-          cidr: "10.20.10.0/24",
-          name: "vpe-zone-1",
-          resource_group: "management-rg",
-          network_acl: "management",
-          public_gateway: false,
-          has_prefix: true,
-        },
-
-        {
-          vpc: "workload",
-          zone: 1,
-          cidr: "10.40.10.0/24",
-          name: "vsi-zone-1",
-          network_acl: "workload",
-          resource_group: "workload-rg",
-          public_gateway: false,
-          has_prefix: true,
-        },
-      ]}
-      securityGroups={[
-        {
-          name: "1",
-          vpc: "workload",
-        },
-        {
-          name: "2",
-          vpc: "workload",
-        },
-        {
-          name: "3",
-          vpc: "management",
-        },
-      ]}
+    <VsiLoadBalancerForm
       invalidCallback={invalidCallback}
       invalidTextCallback={invalidTextCallback}
+      resourceGroups={["a", "b"]}
+      vpcList={["management", "edge-vpc"]}
+      securityGroups={[
+        {
+          vpc: "management",
+          name: "management-vpe",
+          resource_group: "management-rg",
+          rules: [],
+        },
+        {
+          vpc: "workload",
+          name: "workload-vpe",
+          resource_group: "workload-rg",
+          rules: [],
+        },
+        {
+          vpc: "management",
+          name: "management-vsi",
+          resource_group: "management-rg",
+          rules: [],
+        },
+      ]}
+      vsiDeployments={[
+        {
+          kms: "slz-kms",
+          encryption_key: "slz-vsi-volume-key",
+          image: "ibm-ubuntu-18-04-6-minimal-amd64-2",
+          profile: "cx2-4x8",
+          name: "management-server",
+          security_groups: ["management-vpe-sg"],
+          ssh_keys: ["slz-ssh-key"],
+          subnets: ["vsi-zone-1", "vsi-zone-2", "vsi-zone-3"],
+          vpc: "management",
+          vsi_per_subnet: 2,
+          resource_group: "slz-management-rg",
+        },
+      ]}
     />
   );
 }
