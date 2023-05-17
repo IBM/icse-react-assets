2a2a8a66d53b9c9258ab79f82beedd1b0512f99c
Chris Springstead <chris.p.springstead@gmail.com>
2023-05-17 11:35:27 -0500

    dns record form and story

---
+++ src/components/forms/DnsRecordForm.js
@@ -0,0 +1,247 @@
+import React, { Component } from "react";
+import {
+  buildFormDefaultInputMethods,
+  buildFormFunctions,
+} from "../component-utils";
+import PropTypes from "prop-types";
+import { IcseFormGroup } from "../Utils";
+import { IcseNameInput, IcseTextInput } from "../Inputs";
+import { IcseSelect } from "../Dropdowns";
+import { NumberInput } from "@carbon/react";
+import { isRangeInvalid } from "../../lib/iam-utils";
+
+/**
+ * DnsRecordForm
+ * @param {Object} props
+ * @param {configDotJson} props.configDotJson config dot json
+ */
+class DnsRecordForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...this.props.data };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    buildFormDefaultInputMethods(this);
+    buildFormFunctions(this);
+  }
+
+  // Handle input change
+  handleInputChange = (event) => {
+    let { name, value } = event.target;
+    let cluster = { ...this.state };
+    this.setState(handleClusterInputChange(name, value, cluster));
+  };
+
+  render() {
+    let dnsComponent = this.props.isModal
+      ? "new-dns-record"
+      : this.props.data.name;
+    return (
+      <>
+        <IcseFormGroup>
+          <IcseNameInput
+            id={this.state.name + "-name"}
+            labelText="DNS Record Name"
+            componentName={dnsComponent}
+            value={this.state.name}
+            onChange={this.handleInputChange}
+            invalidCallback={() =>
+              this.props.invalidCallback(this.state, this.props)
+            }
+            invalidText={this.props.invalidTextCallback(this.state, this.props)}
+            helperTextCallback={() =>
+              this.props.helperTextCallback(this.state, this.props)
+            }
+            className="fieldWidthSmaller"
+          />
+          <IcseSelect
+            name="dns_zone"
+            formName={dnsComponent + "-dns-zone"}
+            labelText="DNS Zone"
+            groups={this.props.dnsZones}
+            value={this.state.dns_zones}
+            handleInputChange={this.handleInputChange}
+            invalidText="Select a DNS Zone."
+            className="fieldWidthSmaller"
+          />
+          <IcseSelect
+            name="type"
+            formName={dnsComponent + "-type"}
+            labelText="DNS Record Type"
+            groups={["A", "AAA", "CNAME", "PTR", "TXT", "MX", "SRV"]}
+            value={this.state.type}
+            handleInputChange={this.handleInputChange}
+            invalidText="Select a DNS Record Type."
+            className="fieldWidthSmaller"
+          />
+        </IcseFormGroup>
+        <IcseFormGroup>
+          <IcseTextInput
+            componentName="DNS Record"
+            field="resource_data"
+            labelText="Resource Data"
+            value={this.state.rdata}
+            id={this.state.name + "-rdata"}
+            onChange={this.handleInputChange}
+            invalid={this.props.invalidCallback(this.state, this.props)}
+            invalidText={this.props.invalidTextCallback(this.state, this.props)}
+            className="fieldWidth"
+          />
+          <NumberInput
+            label="Time To Live"
+            id={dnsComponent + "-ttl"}
+            allowEmpty={true}
+            value={Math.round(this.state.ttl)}
+            onChange={this.handleInputChange}
+            name="ttl"
+            hideSteppers={true}
+            min={300}
+            max={2147483647}
+            invalid={isRangeInvalid(this.state.ttl, 300, 2147483647)}
+            invalidText="Must be a whole number within range."
+            className="fieldWidth"
+          />
+          {this.state.typ === "MX" && (
+            <>
+              <NumberInput
+                label="Preference"
+                id={dnsComponent + "-preference"}
+                allowEmpty={false}
+                value={Math.round(this.state.preference)}
+                onChange={this.handleInputChange}
+                name="preference"
+                hideSteppers={true}
+                min={0}
+                max={65535}
+                step={1}
+                invalid={isRangeInvalid(this.state.preference, 0, 65535)}
+                invalidText="Must be a whole number within range."
+                className="fieldWidth"
+              />
+            </>
+          )}
+        </IcseFormGroup>
+        {this.state.type === "SRV" && (
+          <>
+            <IcseFormGroup>
+              <NumberInput
+                label="DNS Record Port"
+                id={dnsComponent + "-port"}
+                allowEmpty={false}
+                value={Math.round(this.state.port)}
+                onChange={this.handleInputChange}
+                name="port"
+                hideSteppers={true}
+                min={1}
+                max={65535}
+                step={1}
+                invalid={isRangeInvalid(this.state.port, 1, 65535)}
+                invalidText="Must be a whole number between 1 and 65535"
+                className="fieldWidthSmaller"
+              />
+              <IcseSelect
+                formName={dnsComponent + "-protocol"}
+                name="dns_record_protocol"
+                groups={["TCP", "UDP"]}
+                value={this.state.protocol}
+                labelText="DNS Record Protocol"
+                handleInputChange={this.handleInputChange}
+                className="fieldWidthSmaller"
+              />
+              <NumberInput
+                label="DNS Record Priority"
+                id={dnsComponent + "-priority"}
+                allowEmpty={false}
+                value={Math.round(this.state.priority)}
+                onChange={this.handleInputChange}
+                name="priority"
+                hideSteppers={true}
+                min={0}
+                max={65535}
+                step={1}
+                invalid={isRangeInvalid(this.state.priority, 0, 65535)}
+                invalidText="Must be a whole number between 0 and 65535"
+                className="fieldWidthSmaller"
+              />
+            </IcseFormGroup>
+            <IcseFormGroup>
+              <IcseTextInput
+                id={dnsComponent + "-service"}
+                componentName={"DNS Record"}
+                field="service"
+                value={this.state.service}
+                onChange={this.handleInputChange}
+                labelText={"DNS Record Service"}
+                invalid={this.state.service.charAt(0) !== "_"}
+                invalidText="Service must start with a '_'."
+                className={"fieldWidthSmaller"}
+              />
+              <NumberInput
+                label="DNS Record Weight"
+                id={dnsComponent + "-weight"}
+                allowEmpty={false}
+                value={Math.round(this.state.weight)}
+                onChange={this.handleInputChange}
+                name="service"
+                hideSteppers={true}
+                min={0}
+                max={65535}
+                step={1}
+                invalid={isRangeInvalid(this.state.weight, 0, 65535)}
+                invalidText="Must be a whole number between 0 and 65535"
+                className="fieldWidthSmaller"
+              />
+            </IcseFormGroup>
+          </>
+        )}
+      </>
+    );
+  }
+}
+
+DnsRecordForm.defaultProps = {
+  data: {
+    name: "",
+    dns_zone: "",
+    type: "",
+    rdata: "",
+    ttl: 300,
+    port: 80,
+    protocol: "",
+    priority: 0,
+    service: "",
+    weight: 0,
+    preference: 0,
+  },
+  invalidCallback: () => {
+    return false;
+  },
+  invalidTextCallback: () => {
+    return "Invalid";
+  },
+  helperTextCallback: () => {
+    return "";
+  },
+  dnsZones: [],
+};
+
+DnsRecordForm.propTypes = {
+  data: PropType.shape({
+    name: PropTypes.string,
+    dns_zone: PropTypes.string,
+    type: PropTypes.string,
+    rdata: PropTypes.string,
+    ttl: PropTypes.number,
+    port: PropTypes.number,
+    protocol: PropTypes.string,
+    priority: PropTypes.number,
+    service: PropTypes.string,
+    weight: PropTypes.number,
+    preference: PropTypes.number,
+  }),
+  invalidCallback: PropTypes.func.isRequired,
+  invalidTextCallback: PropTypes.func.isRequired,
+  helperTextCallback: PropTypes.func.isRequired,
+  dnsZones: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default DnsRecordForm;
---
+++ storybook/src/stories/Forms/DnsRecordForm.stories.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { DnsRecordForm } from "icse-react-assets";
+
+export default {
+  component: DnsRecordForm,
+  title: "Components/Forms/DnsRecordForm",
+  argTypes: {
+    data: {
+      summary: "An optional object",
+      type: { required: true },
+      control: "none",
+    },
+    "data.name": {
+      description: "A string value for the name of the DNS Record",
+      control: "none",
+      type: { required: false }, // required prop or not
+    },
+    "data.dns_zone": {
+      description: "A string value for the DNS zone",
+      control: "none",
+      type: { required: false },
+    },
+    "data.type": {
+      description: "A string value for DNS record type",
+      control: "none",
+      type: { required: false },
+    },
+    "data.rdata": {
+      description: "A string value for DNS record resource data",
+      control: "none",
+      type: { required: false },
+    },
+    "data.ttl": {
+      description: "A number value for DNS record time to live (ttl)",
+      control: "none",
+      type: { required: false },
+    },
+    "data.protocol": {
+      description: "A string value for DNS record protocol",
+      control: "none",
+      type: { required: false },
+    },
+    "data.priority": {
+      description: "A number value for DNS record priority",
+      control: "none",
+      type: { required: false },
+    },
+    "data.service": {
+      description: "A string value for DNS record service",
+      control: "none",
+      type: { required: false },
+    },
+    "data.weight": {
+      description: "A number value for DNS record weight",
+      control: "none",
+      type: { required: false },
+    },
+    "data.preference": {
+      description: "A number value for DNS record preference",
+      control: "none",
+      type: { required: false },
+    },
+    invalidCallback: {
+      description:
+        "A function to determine if the value supplied is invalid and returns a single boolean",
+      type: { required: true },
+      control: "none",
+    },
+    invalidTextCallback: {
+      description:
+        "A function to determine the invalid text displayed to the user and returns the string to display",
+      type: { required: true },
+      control: "none",
+    },
+    helperTextCallback: {
+      description:
+        "A function to display helperText for the specified DNS record name",
+      type: { required: true },
+      control: "none",
+    },
+    dnsZones: {
+      description: "An array (string) of DNS zones",
+      type: { required: true },
+      control: "none",
+    },
+  },
+};
+
+const DnsRecordFormStory = () => {
+  function validName(str) {
+    const regex = /^[A-z]([a-z0-9-]*[a-z0-9])?$/s;
+    if (str) return str.match(regex) !== null;
+    else return false;
+  }
+
+  function invalidCallback(stateData, componentProps) {
+    return !validName(stateData.name);
+  }
+
+  function helperTextCallback(stateData, componentProps) {
+    return "<prefix>-" + stateData.name;
+  }
+
+  function invalidTextCallback(stateData, componentProps) {
+    return contains(["foo", "bar"], stateData.name)
+      ? `Cluster name ${stateData.name} already in use.`
+      : `Invalid Name. Must match the regular expression: /^[A-z]([a-z0-9-]*[a-z0-9])?$/i`;
+  }
+
+  return (
+    <DnsRecordForm
+      invalidCallback={invalidCallback}
+      invalidTextCallback={invalidTextCallback}
+      helperTextCallback={helperTextCallback}
+      dnsZones={["zone1", "zone2", "zone3"]}
+    />
+  );
+};
+
+export const Default = DnsRecordFormStory.bind({});
