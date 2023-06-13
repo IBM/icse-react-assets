cc191231206f28383d50e0814cf524a2cbfc45ea
Jennifer Valle <jvallexm@gmail.com>
2023-06-13 10:58:49 -0400

    Craig 555: Refactor Encryption Key Form (#136)
    * refactor: encryption key
    
    * bump: version
    
    * alignModal

--- storybook/src/stories/Forms/EncryptionKey.stories.js
+++ storybook/src/stories/Forms/EncryptionKey.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { contains } from "lazy-z";
-import { EncryptionKeyForm } from "icse-react-assets";
+import { EncryptionKeyForm, IcseModal } from "icse-react-assets";
 
 export default {
   component: EncryptionKeyForm,
@@ -102,7 +102,7 @@ const EncryptionKeyFormStory = () => {
         force_delete: false,
         dual_auth_delete: true,
         key_ring: "",
-        endpoint: "private"
+        endpoint: "private",
       }}
       invalidCallback={invalidCallback}
       invalidTextCallback={invalidTextCallback}
@@ -112,4 +112,59 @@ const EncryptionKeyFormStory = () => {
   );
 };
 
+const EncryptionKeyModalFormStory = () => {
+  function validName(str) {
+    const regex = /^[A-z]([a-z0-9-]*[a-z0-9])?$/s;
+    if (str) return str.match(regex) !== null;
+    else return false;
+  }
+
+  function invalidCallback(stateData, componentProps) {
+    return (
+      !validName(stateData.name) || contains(["foo", "bar"], stateData.name)
+    );
+  }
+
+  function invalidRingCallback(stateData, componentProps) {
+    return stateData.key_ring === "" ? false : !validName(stateData.key_ring);
+  }
+
+  function invalidTextCallback(stateData, componentProps) {
+    return contains(["foo", "bar"], stateData.name)
+      ? `Key name ${stateData.name} already in use.`
+      : `Invalid Key Name. Must match the regular expression: /^[A-z]([a-z0-9-]*[a-z0-9])*$/s`;
+  }
+
+  let invalidRingText = `Invalid Key Ring Name. Must match the regular expression: /^[A-z]([a-z0-9-]*[a-z0-9])*$/s`;
+
+  return (
+    <IcseModal
+      onRequestSubmit={() => {}}
+      onRequestClose={() => {}}
+      open
+      heading="Encryption Key Modal"
+      primaryButtonDisabled
+      primaryButtonText="Submit"
+    >
+      <EncryptionKeyForm
+        data={{
+          name: "test-encryption-key",
+          rotation: 12,
+          root_key: false,
+          force_delete: false,
+          dual_auth_delete: true,
+          key_ring: "",
+          endpoint: "private",
+        }}
+        invalidCallback={invalidCallback}
+        invalidTextCallback={invalidTextCallback}
+        invalidRingCallback={invalidRingCallback}
+        invalidRingText={invalidRingText}
+        isModal
+      />
+    </IcseModal>
+  );
+};
+
 export const Default = EncryptionKeyFormStory.bind({});
+export const Modal = EncryptionKeyModalFormStory.bind({});
