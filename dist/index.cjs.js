972f176f86af428b15bc6456bb1a6cc91ebbddb5
Isaac Eichelberger <isaac.eichelberger@gmail.com>
2023-06-19 12:07:40 -0500

    routing table form refactor (#151)
    Co-authored-by: Isaac Eichelberger <Isaac.Eichelberger@ibm.com>

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -6929,9 +6929,9 @@ class RoutingTableForm extends React.Component {
     };
     if (this.props.isModal) this.state.routes = [];
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
     buildFormFunctions(this);
     buildFormDefaultInputMethods(this);
-    this.handleToggle = this.handleToggle.bind(this);
   }
 
   /**
@@ -6940,15 +6940,7 @@ class RoutingTableForm extends React.Component {
    * @param {*} value value to update
    */
   handleInputChange(event) {
-    let nextState = {
-      ...this.state
-    };
-    let {
-      name,
-      value
-    } = event.target;
-    nextState[name] = value;
-    this.setState(nextState);
+    this.setState(this.eventTargetToNameAndValue(event));
   }
   handleToggle(name) {
     this.setState(this.toggleStateBoolean(name, this.state));
