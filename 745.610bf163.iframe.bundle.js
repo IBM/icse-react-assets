1b15d905e4df9248abeeb93eff1c7550d9218a42
jvallexm <jvallexm@gmail.com>
2023-10-03 15:14:59 -0400

    Updates

--- 745.610bf163.iframe.bundle.js
+++ 745.610bf163.iframe.bundle.js
@@ -1,1 +1,16 @@
-"use strict";(self.webpackChunkintro_storybook_react_template=self.webpackChunkintro_storybook_react_template||[]).push([[745],{"./node_modules/react-dom/client.js":function(__unused_webpack_module,exports,__webpack_require__){var m=__webpack_require__("./node_modules/react-dom/index.js");exports.createRoot=m.createRoot,exports.hydrateRoot=m.hydrateRoot}}]);
+"use strict";
+(self.webpackChunkintro_storybook_react_template =
+  self.webpackChunkintro_storybook_react_template || []).push([
+  [745],
+  {
+    "./node_modules/react-dom/client.js": function (
+      __unused_webpack_module,
+      exports,
+      __webpack_require__,
+    ) {
+      var m = __webpack_require__("./node_modules/react-dom/index.js");
+      (exports.createRoot = m.createRoot),
+        (exports.hydrateRoot = m.hydrateRoot);
+    },
+  },
+]);
