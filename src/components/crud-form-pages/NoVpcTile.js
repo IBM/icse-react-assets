13886307410ca5be938bf79a795ac84912b99c20
Connor Terech <Connor.Terech@ibm.com>
2023-10-17 10:24:17 -0400

    text of noVpc tile

--- src/components/crud-form-pages/NoVpcTile.js
+++ src/components/crud-form-pages/NoVpcTile.js
@@ -6,7 +6,7 @@ import "./no-secrets-manager-tile.css";
 export const NoVpcTile = (resource) => {
   return (
     <Tile className="tileBackground displayFlex alignItemsCenter wrap marginTop">
-      <CloudAlerting size="24" className="iconMargin" /> No VPC instances have
+      <CloudAlerting size="24" className="iconMargin" /> No VPCs have
       been created. Create one from the
       <a className="no-vpc-link" href="/form/vpcs">
         Virtual Private Clouds Page
