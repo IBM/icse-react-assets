98bcf58165ec0a324fe973ccfe4103b66b647cdb
Ayman Naji <ayman.naji@ibm.com>
2023-11-06 13:48:28 -0800

    added hotfix top allow hiding title button for classic ssh key, removing requirement of RG for classic ssh key
    Signed-off-by: Ayman Naji <ayman.naji@ibm.com>

--- src/components/crud-form-pages/SshKeys.js
+++ src/components/crud-form-pages/SshKeys.js
@@ -18,6 +18,7 @@ export const SshKeys = (props) => {
       onSubmit={props.onSubmit}
       propsMatchState={props.propsMatchState}
       forceOpen={props.forceOpen}
+      hideFormTitleButton={props.overrideTile ? true : false}
       deleteDisabled={props.deleteDisabled}
       deleteDisabledMessage="SSH Key currently in use"
       innerFormProps={{
@@ -58,7 +59,7 @@ SshKeys.propTypes = {
   onSubmit: PropTypes.func.isRequired,
   propsMatchState: PropTypes.func.isRequired,
   forceOpen: PropTypes.func.isRequired,
-  resourceGroups: PropTypes.array.isRequired,
+  resourceGroups: PropTypes.array, //not required for classic
   invalidCallback: PropTypes.func.isRequired,
   invalidTextCallback: PropTypes.func.isRequired,
   craig: PropTypes.shape({}),
